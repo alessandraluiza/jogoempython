65420844ae5c5c6d3f146890be20049e4788d440
alessandraluiza <aleluizasg@gmail.com>
2023-12-18 13:54:04 -0300

    Atualização

--- adivinhacao.py
+++ adivinhacao.py
@@ -9,10 +9,15 @@ chute_str = input("Digite o seu numero: ")
 print("Você digitou ",chute_str)
 
 chute = int(chute_str)
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
 
-if (numero_secreto == chute):
+if (acertou):
     print("Você acertou")
 else:
-    print("Você errou")
-
+    if(maior):
+        print("Você errou! O seu chute foi maior do que o número secreto.")
+    elif (menor):
+        print("Você errou! O seu chute foi menor do que o número secreto.")
 print("Fim de jogo")
